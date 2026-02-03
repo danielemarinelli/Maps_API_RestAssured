41e030925eaf425561f9bcd20d90c12ad9d99c10
Daniele <danielemarinelli1387@gmail.com>
2026-02-03 17:46:16 +0100

    added partial POJO classes for GetCourses and courses

---
+++ src/main/java/POJO/Courses.java
@@ -0,0 +1,36 @@
+package POJO;
+
+public class Courses {
+
+    public String webAutomation;
+    public String api;
+    public String mobile;
+
+    public void setWebAutomation(String webAutomation) {
+        this.webAutomation = webAutomation;
+    }
+
+    public void setApi(String api) {
+        this.api = api;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getWebAutomation() {
+        return webAutomation;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public String getApi() {
+        return api;
+    }
+
+
+
+
+}
---
+++ src/main/java/POJO/GetCourses.java
@@ -0,0 +1,112 @@
+package POJO;
+
+public class GetCourses {
+
+    //  when you launch oAuthTest, the courses are extracted as JSON like below:
+    /*  use the like --->    https://jsoneditoronline.org/     to understand the JSON Structure and the nested objects
+    WE HAVE six <k,v> so let's create 6  variables  (courses has nested objects)
+
+    {
+  "instructor": "RahulShetty",
+  "url": "rahulshettycademy.com",
+  "services": "projectSupport",
+  "expertise": "Automation",
+  "courses": {
+    "webAutomation": [
+      {
+        "courseTitle": "Selenium Webdriver Java",
+        "price": "50"
+      },
+      {
+        "courseTitle": "Cypress",
+        "price": "40"
+      },
+      {
+        "courseTitle": "Protractor",
+        "price": "40"
+      }
+    ],
+    "api": [
+      {
+        "courseTitle": "Rest Assured Automation using Java",
+        "price": "50"
+      },
+      {
+        "courseTitle": "SoapUI Webservices testing",
+        "price": "40"
+      }
+    ],
+    "mobile": [
+      {
+        "courseTitle": "Appium-Mobile Automation using Java",
+        "price": "50"
+      }
+    ]
+  },
+  "linkedIn": "https://www.linkedin.com/in/rahul-shetty-trainer/"
+}
+
+    */
+
+    private String instructor;
+    private String url;
+    private String services;
+    private String expertise;
+    private Courses courses;
+    private String linkedIn;
+
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getServices() {
+        return services;
+    }
+
+    public String getExpertise() {
+        return expertise;
+    }
+
+    public Courses getCourses() {
+        return courses;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setServices(String services) {
+        this.services = services;
+    }
+
+    public void setExpertise(String expertise) {
+        this.expertise = expertise;
+    }
+
+    public void setCourses(Courses courses) {
+        this.courses = courses;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+
+
+
+
+
+}
