9807140adf1b1e0bd28e0ea22235d7da56700026
Daniele <danielemarinelli1387@gmail.com>
2026-02-04 10:38:51 +0100

    deserialization using pojo classes

--- src/test/java/oAuthTest.java
+++ src/test/java/oAuthTest.java
@@ -1,11 +1,15 @@
+import POJO.Api;
 import POJO.GetCourses;
+import POJO.WebAutomation;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
 
 @Test
@@ -60,11 +64,38 @@ public class oAuthTest {
                     .get("https://rahulshettyacademy.com/oauthapi/getCourseDetails").as(GetCourses.class);
 
             //  Instead of using JSON Parse, easiest way is with pojos
-        System.out.println("---- with POJO-------");
-        System.out.println(allCoursesInfo.getLinkedIn());
-        System.out.println(allCoursesInfo.getExpertise());
+            System.out.println("---- with POJO-------");
+            System.out.println(allCoursesInfo.getLinkedIn());
+            System.out.println(allCoursesInfo.getExpertise());
+            System.out.println("With fixed index: "+allCoursesInfo.getCourses().getApi().get(1).getCourseTitle());
 
+            //get the price of the API course: 'Rest Assured Automation using Java' without hardcoding the index (JSON file can be always updated)
+            List<Api> APIs =  allCoursesInfo.getCourses().getApi();
+            for(int i=0; i<APIs.size();i++){
+                if(APIs.get(i).getCourseTitle().equalsIgnoreCase("Rest Assured Automation using Java")){
+                    System.out.println("Price is: "+APIs.get(i).getPrice());
+                }
 
+            }
+
+            // get all courses titles of webAutomation array and compare with expected courses titles
+            // Let's create array of expected strings titles or list of expected strings titles, what you prefer
+            //String[] expectedArrayTitles = {"Selenium Webdriver Java","Cypress","Protractor"};
+            List<String> expectedTitles = List.of("Selenium Webdriver Java", "Cypress", "Protractor");
+            List<WebAutomation> webAuto =  allCoursesInfo.getCourses().getWebAutomation();
+            System.out.println("Courses under WebAutomation are listed below:");
+            ArrayList<String> actualTitleInJSON = new ArrayList<>();
+            for(int i=0; i<webAuto.size();i++) {
+                actualTitleInJSON.add(webAuto.get(i).getCourseTitle());
+                System.out.println(webAuto.get(i).getCourseTitle());
+
+            }
+            System.out.println(actualTitleInJSON);
+            System.out.println(expectedTitles);
+            Assert.assertEquals(expectedTitles, actualTitleInJSON);
+            //if using Array of Strings remember to convert Array into List and then compare the two Lists
+            // List<String> expTitles = Arrays.asList(expectedArrayTitles);
+            // Assert.assertEquals(expTitles, actualTitleInJSON);
     }
 
 
