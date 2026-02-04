867a6a68057451778f9191abba4dfa5579bdef68
Daniele <danielemarinelli1387@gmail.com>
2026-02-04 12:14:33 +0100

    refactor test with SpecBuilder methods

--- src/test/java/Maps_POJO.java
+++ src/test/java/Maps_POJO.java
@@ -1,6 +1,12 @@
 import POJO4MapsTest.AddPlace;
 import POJO4MapsTest.Location;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
@@ -32,6 +38,7 @@ public class Maps_POJO {
         loc.setLat(-38.383494);
         loc.setLng(53.427362);
         ap.setLocation(loc);
+
         String response = given().log().all().queryParam("key","qaclick123")
                 .headers("Content-Type","application/json")
                 .body(ap)
@@ -43,4 +50,54 @@ public class Maps_POJO {
         System.out.println("######### Response: " + response);
 
     }
+
+
+    @Test
+    public void deserializationTestWithSpecBuilder(){
+
+        // Details generic for every Maps API requests, we can collect them in a RequestSpecBuilder method
+        // and ResponseSpecBuilder for API generic responses
+        RequestSpecification reqSpec = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").addQueryParam("key","qaclick123")
+                        .setContentType(ContentType.JSON).build();
+        ResponseSpecification responseSpec = new ResponseSpecBuilder().expectStatusCode(200)
+                .expectContentType(ContentType.JSON).build();
+        RestAssured.useRelaxedHTTPSValidation();   // ---> to bypass SSL verification
+
+        AddPlace ap = new AddPlace();
+        ap.setAccuracy(100);
+        ap.setAddress("Piazza Venezia 14");
+        ap.setLanguage("French");
+        ap.setName("Spiderman");
+        ap.setPhoneNumber("+(39)0010001533");
+        ap.setWebsite("www.nfl.com");
+        List<String> ty = new ArrayList<>();
+        ty.add("rb");
+        ty.add("wr");
+        ty.add("lb");
+        ap.setTypes(ty);
+        Location loc = new Location();
+        loc.setLat(-38.383494);
+        loc.setLng(53.427362);
+        ap.setLocation(loc);
+
+        // create RequestSpecification object and append it to when method
+        RequestSpecification res = given().log().all().spec(reqSpec)
+                .body(ap);
+        // The code is more readable
+        Response r = res.when()
+                .post("/maps/api/place/add/json")
+                .then()
+                .spec(responseSpec)
+                .extract().response();
+
+
+                String response = r.asString();
+
+        System.out.println("######### Response: " + response);
+
+    }
+
+
+
+
 }
