c63441a230bf3c25f3bfcd722eb96c73c0023263
Daniele <danielemarinelli1387@gmail.com>
2026-03-17 18:01:29 +0100

    Added final step for OAuth2.0 test (step1)

--- src/test/java/oAuthTest.java
+++ src/test/java/oAuthTest.java
@@ -105,12 +105,22 @@ public class oAuthTest {
     // let's follow bottom to top approach:
     public void GetCoursesOAuth2(){
 
+            //step1
+            // DEV will share the URL where USER must sign in manually with Google account credentials.
+        // once the sign in is successfully, there will be a link as below, of the app to test, where
+        // contains the access code that we need. The mentioned code is a queryParameter and you can see it in the url
+        // its this one from the URL -->  code=4%2FvAHBQUZU6o4WJ719NrGBzSELBFVBI9XbxvOtYpmYpeV47bFVExkaxWaF_XR14PHtTZf7ILSEeamywJKwo_BYs9M
+        String url ="https://rahulshettyacademy.com/getCourse.php?state=verifyfjdss&code=4%2FvAHBQUZU6o4WJ719NrGBzSELBFVBI9XbxvOtYpmYpeV47bFVExkaxWaF_XR14PHtTZf7ILSEeamywJKwo_BYs9M&scope=email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=0&session_state=0c32992f0d47e93d273922018ade42d1072b9d1f..a35c&prompt=none#";
 
+        String partialcode=url.split("code=")[1];
 
+        String code=partialcode.split("&scope")[0];
 
+        System.out.println(code);
 
             //step2    params given from the DEVELOPER in documentation
-            String accessTokenResponse = given().queryParams("code","")
+            String accessTokenResponse = given().urlEncodingEnabled(false) //keeps all the special characters in the token without converting them
+                    .queryParams("code",code)
                     .queryParams("client_id","692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
                     .queryParams("client_secret","erZOWM9g3UtwNRj340YYaK_W")
                     .queryParams("redirect_uri","https://rahulshettyacademy.com/getCourse.php")
