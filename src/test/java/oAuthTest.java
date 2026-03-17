9f25b2a8612ef2baa4d615c1b955fb2091b8f69f
Daniele <danielemarinelli1387@gmail.com>
2026-03-17 15:22:48 +0100

    Add partial test for OAuth2.0 (step2)

--- src/test/java/oAuthTest.java
+++ src/test/java/oAuthTest.java
@@ -105,8 +105,24 @@ public class oAuthTest {
     // let's follow bottom to top approach:
     public void GetCoursesOAuth2(){
 
+
+
+
+
+            //step2    params given from the DEVELOPER in documentation
+            String accessTokenResponse = given().queryParams("code","")
+                    .queryParams("client_id","692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
+                    .queryParams("client_secret","erZOWM9g3UtwNRj340YYaK_W")
+                    .queryParams("redirect_uri","https://rahulshettyacademy.com/getCourse.php")
+                    .queryParams("grant_type","authorization_code")
+                    .when().log().all()
+                    .post("https://www.googleapis.com/oauth2/v4/token").asString();
+
+            JsonPath js = new JsonPath(accessTokenResponse);
+            String accessToken = js.getString("access_token");  // pass the accessToken to step3
+
             //step3
-            String resp = given().queryParam("access token","")
+            String resp = given().queryParam("access token",accessToken)
                     .when()
                     .get("https://rahulshettyacademy.com/getCourse.php").asString();
 
